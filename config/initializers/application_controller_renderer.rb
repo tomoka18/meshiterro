b9e883466ecc7f71bcb8b3c560985cc264fc51cd
Tomoka18 <gudegude18@gmail.com>
2021-06-14 18:41:28 +0000

    [Finish4-5]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '910bc3ff61714b770cabd9337796eb598351c91b88a2f98bad95c14904c8a776469eae276b2549624c5a10af1bc09c782ac651772c1f2f4e9171d0de5a46301d'
